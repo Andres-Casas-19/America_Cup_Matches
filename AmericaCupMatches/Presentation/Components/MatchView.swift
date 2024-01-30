6945ae0df1070187345900533e19dff6a875592e
Andrés Casas <andres.casas@globant.com>
2024-01-30 15:39:34 -0500

    Adjust match view component

--- AmericaCupMatches/Presentation/Components/MatchView.swift
+++ AmericaCupMatches/Presentation/Components/MatchView.swift
@@ -8,54 +8,92 @@
 import SwiftUI
 
 struct MatchView: View {
+
+  // MARK: - Properties
   let matchCell: MatchCell
+  private var widthImageAndText: CGFloat = 120
+  private var heightImage: CGFloat = 80
+  private var spacing: CGFloat = 12
+  private var cellTitleVerticalSpace: CGFloat = 8
   
-  init(_ match: MatchCell) {
+  init(_ match: MatchCell,
+       spacing: CGFloat? = nil) {
     self.matchCell = match
+    self.spacing = spacing ?? self.spacing
+  }
+  
+  private init(_ matchCell: MatchCell,
+               widthImageAndText: CGFloat = 120,
+               heightImage: CGFloat = 80,
+               spacing: CGFloat,
+               cellTitleVerticalSpace: CGFloat = 8) {
+    self.matchCell = matchCell
+    self.widthImageAndText = widthImageAndText
+    self.heightImage = heightImage
+    self.spacing = spacing
+    self.cellTitleVerticalSpace = cellTitleVerticalSpace
   }
 
     var body: some View {
       VStack {
+
+        Spacer()
+          .frame(height: 8)
+
         Text(matchCell.title)
           .bold()
       
-          VStack(spacing: 12) {
+          VStack(spacing: spacing) {
             
-            HStack(spacing: 12) {
+            HStack(spacing: spacing) {
               Image(matchCell.localTeam.flag)
                 .resizable()
-                .frame(width: 120, height: 80)
+                .frame(width: widthImageAndText, height: heightImage)
               
               VStack {
                 Text(matchCell.result)
                 Text(matchCell.matchStatus.rawValue)
                   .bold()
               }
+
               Image(matchCell.visitorTeam.flag)
                 .resizable()
-                .frame(width: 120, height: 80)
+                .frame(width: widthImageAndText, height: heightImage)
             }
             
-            HStack(spacing: 45) {
-              Spacer()
-              
+            HStack(spacing: spacing) {
+
               Text(matchCell.localTeam.name)
                 .multilineTextAlignment(.center)
-                .frame(width: 120, alignment: .center)
+                .frame(width: widthImageAndText, alignment: .center)
               
-              Spacer()
+              Text(matchCell.matchStatus.rawValue)
+                .bold()
+                .hidden()
 
               Text(matchCell.visitorTeam.name)
                 .multilineTextAlignment(.center)
-                .frame(width: 120, alignment: .center)
+                .frame(width: widthImageAndText, alignment: .center)
               
-              Spacer()
             }
+            .padding(.trailing, spacing * 2)
+            .padding(.leading, spacing * 2)
           }
-          .padding(.top, 8)
+          .padding(.top, cellTitleVerticalSpace)
+
           Spacer()
+          .frame(height: 8)
       }
     }
+  
+  @inlinable func widthImages(widthImageAndText: CGFloat? = nil,
+                              heightImage: CGFloat? = nil) -> some View {
+    MatchView(self.matchCell,
+              widthImageAndText: widthImageAndText ?? self.widthImageAndText,
+              heightImage: heightImage ?? self.heightImage,
+              spacing: self.spacing)
+  }
+  
 }
 
 struct MatchView_Previews: PreviewProvider {
